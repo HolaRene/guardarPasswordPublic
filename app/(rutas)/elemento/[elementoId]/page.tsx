2be84820cb640cec260f97b463f0fd6cc5818e30
Jaime <176935470+HolaRene@users.noreply.github.com>
2025-03-07 18:50:14 -0600

    se cambiaron en [elememtId]

--- app/(rutas)/elemento/[elementoId]/page.tsx
+++ app/(rutas)/elemento/[elementoId]/page.tsx
@@ -6,20 +6,23 @@ import { redirect } from 'next/navigation'
 export default async function ElementoEditar({
   params,
 }: Readonly<{ params: { elementoId: string } }>) {
-  const { elementoId } = await params
+  const { elementoId } = params // ❌ No usar await aquí
+
   const sesion = await getServerSession()
   if (!sesion || !sesion.user?.email) {
     return redirect('/')
   }
+
   // Obtener el elemento a editar de la base de datos
   const elemento = await db.element.findUnique({
     where: { id: elementoId },
   })
-  // console.log(elemento)
+
   // Si el elemento no existe, redireccionar al inicio
   if (!elemento) {
     return redirect('/')
   }
+
   return (
     <div>
       <h1 className='mb-2'>Editando Elemento</h1>
